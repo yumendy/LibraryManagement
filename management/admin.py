01b686aa620249ed29a3603d2715ea250eafa7d7
yumendy <yumendy@163.com>
2016-01-25 19:19:29 +0800

    python3 support

--- management/admin.py
+++ management/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from models import *
+from management.models import *
 
 
 class MyUserInline(admin.StackedInline):
--- management/urls.py
+++ management/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-import views
+from management import views
 
 urlpatterns = [
     url(r'^$', views.index, name='homepage'),
